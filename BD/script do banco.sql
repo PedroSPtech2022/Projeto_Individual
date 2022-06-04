297bca67c6012b7d3cbcffd41d85d7ca308ba080
pedrosptech2022 <pedro.cordeironeto@sptech.school>
2022-06-04 14:48:51 -0300

    Adicionando Modelagem Logica e Tabela de Contato no Script SQL

--- BD/script do banco.sql
+++ BD/script do banco.sql
@@ -2,42 +2,42 @@ CREATE DATABASE champions;
 USE champions;
 
 CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(45),
+	email VARCHAR(45),
+	senha VARCHAR(15),
 	nomeTIMEF VARCHAR (50),
     fkTimeUCL int,
-    foreign key (fkTimeUCl) references TIMESUCL (idTime)
-);						
+    foreign key (fkTimeUCl) references TIMESUCL (idTimeUCL)
+);
+						
+insert into usuario values (null,'Pedro','pedro@ucl.com','pedrinho123','São Paulo',2001),
+                           (null,'Wilker','wilker@sptech.school','1234','São Paulo',2004),
+                           (null,'Thais de frança','thais@sptech.school','1234','São Paulo',2012),
+                           (null,'Mauricio','mauricio@sptech.school','123','Corinthians',2006),
+                           (null,'Eduardo Begattini','eduardobegattini@gmail.com','54321','Time Feminino',2016),
+                           (null,'Mateus','mateus@sptech.school','12345','Corinthians',2004),
+                           (null,'Luigi','lulu@gmail.com','12345','Palmeiras',2000),
+                           (null,'Leonardo Pinho','leonardo.pinho@sptech.school','1234','Barcelona',2004),
+                           (null,'Gustavo','gusta.goncalves@hotmail.com','Flamengo','12345',2002),
+                           (null,'Irineu','irineu@gmail.com','1234','Criciuma',2016),
+                           (null,'Gabriel Bordin','biel@gmail.com','123','Santos',2014),
+                           (null,'Nicolas','nick@gmail.com','1122','Borussia Dortmound',2012),
+                           (null,'Rafa','rafael.oliveira@sptech.school','rafa','Corinthians',2006),
+                           (null,'Ruy','ruy@gmail.com','1234','Vasco da Gama',2002),
+                           (null,'Michelly','Michelly@gmail.com','1234','Corinthians',2002),
+                           (null,'Rafaela','rafaela.dias@sptech.school','123','Corinthians',2000),
+                           (null,'Gabriel','gabriel.antonio@hotmail.com','ascd123','Corinthians',2007),
+                           (null,'Patrick','patrick@gmail.com','1234','Corinthians',2007),
+                           (null,'Lourenzo Mota','lourenzo.silva@gmail.com','1234567','Corinthians',2000),
+                           (null,'Ezequiel','ezequiel@gmail.com','0907','Santos',2004),
+                           (null,'Vitinho','victorlage@gmail.com','ODLA','Flamengo',2015),
+                           (null,'Fabio','fabo@gmail.com','123','Corinthians',2002);
+                           
 select * from usuario;
-insert into usuario values (1,'Pedro','pedro@ucl.com','pedrinho123','São Paulo', 2001),
-                           (2,'Wilker','wilker@sptech.school','1234','São Paulo', 2004),
-                           (3,'Thais de frança','thais@sptech.school','1234','São Paulo',2012),
-                           (4,'Mauricio','mauricio@sptech.school','123','Corinthians',2006),
-                           (5,'Eduardo Begattini','eduardobegattini@gmail.com','54321','Time Feminino',2016),
-                           (6,'Mateus','mateus@sptech.school','12345','Corinthians',2004),
-                           (7,'Luigi','lulu@gmail.com','12345','Palmeiras',2000),
-                           (8,'Leonardo Pinho','leonardo.pinho@sptech.school','Barcelona',2004),
-                           (9,'Gustavo','gusta.goncalves@hotmail.com','Flamengo','12345',2002),
-                           (10,'Irineu','irineu@gmail.com','1234','Criciuma',2016),
-                           (11,'Gabriel Bordin','biel@gmail.com','123','Santos',2014),
-                           (12,'Nicolas','nick@gmail.com','1122','Borussia Dortmound',2012),
-                           (13,'Rafa','rafael.oliveira@sptech.school','rafa','Corinthians',2006),
-                           (14,'Ruy','ruy@gmail.com','1234','Vasco da Gama',2002),
-                           (15,'Michelly','Michelly@gmail.com','1234','Corinthians',2002),
-                           (16,'Rafaela','rafaela.dias@sptech.school','123','Corinthians',2000),
-                           (17,'Gabriel','gabriel.antonio@hotmail.com','ascd123','Corinthians',2007),
-                           (18,'Patrick','patrick@gmail.com','1234','Corinthians',2007),
-                           (19,'Lourenzo Mota','lourenzo.silva@gmail.com','1234567','Corinthians',2000),
-                           (20,'Ezequiel','ezequiel@gmail.com','0907','Santos',2004),
-                           (21,'Vitinho','victorlage@gmail.com','ODLA','Flamengo',2015),
-                           (22,'Fabio','fabo@gmail.com','123','Corinthians',2002);
-
-select TIMESUCL.nomeTime, count(usuario.id) as 'Quantidade de Votos' from Usuario join TIMESUCL on fkTimeUCL = idTIme group by TIMESUCL.nomeTime;  
 
 CREATE TABLE TIMESUCL (
-idTime INT PRIMARY KEY AUTO_INCREMENT,
+idTimeUCL INT PRIMARY KEY AUTO_INCREMENT,
 nomeTime VARCHAR(45)
 )auto_increment = 2000;
 
@@ -63,6 +63,15 @@ INSERT INTO TIMESUCL VALUES (null,'Real Madrid'),
                             (null,'Steaute'),
                             (null,'Olympique de Marseillie'),
                             (null,'Nottingham Forest');
-select * from TIMESUCL;        
+select * from TIMESUCL;                        
+select TIMESUCL.nomeTime, count(usuario.id) as 'Quantidade de Votos' from Usuario join TIMESUCL on fkTimeUCL = idTIme group by TIMESUCL.nomeTime; 
 
-select * from usuario;                   
+create table Contato (
+idContato int primary key auto_increment,
+descricao varchar (300),
+fkUsuario int,
+foreign key (fkUsuario) references Usuario (idUsuario)
+)auto_increment= 1000;
+
+insert into Contato values (null,'Site esta ficando execelente parabéns pelo projeto',2);
+select * from Contato;
